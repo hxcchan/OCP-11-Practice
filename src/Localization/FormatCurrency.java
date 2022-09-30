aca09b9044a9ddbedaa8a689f75bc725fd48c356
Song Han <hansong9547@gmail.com>
2022-09-29 23:06:30 -0400

    commit

---
+++ src/Localization/FormatCurrency.java
@@ -0,0 +1,22 @@
+package Localization;
+
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Locale;
+
+public class FormatCurrency {
+    public static void main(String[] args) {
+        var amount = 567.87;
+        Locale locale = Locale.getDefault();
+        Locale chinaLocale = Locale.CHINA;
+
+        Currency currency = Currency.getInstance(locale);
+        System.out.println(currency.getDisplayName());
+        System.out.println(currency.getSymbol());
+
+        NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(chinaLocale);
+        String formattedCurrency = currencyFormatter.format(amount);
+        System.out.println(formattedCurrency);
+    }
+}
+
